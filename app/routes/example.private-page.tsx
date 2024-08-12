eda35166bc285cdc2f97ea9d6f1da0ae34fbfa99
Patrick McCullough <patrick@descentrix.co>
2024-08-12 12:44:27 -0400

    single quotes

--- app/routes/example.private-page.tsx
+++ app/routes/example.private-page.tsx
@@ -1,4 +1,4 @@
-import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
+import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/node';
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { authenticator } from "~/services/auth.server";
